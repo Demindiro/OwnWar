8a3ccdde0df6f99f45af3cb7dbacd044bf068e4a
David Hoppenbrouwers <david@salt-inc.org>
2020-09-15 20:50:56 +0200

    Add collision box to vehicle

--- vehicles/vehicle.gd
+++ vehicles/vehicle.gd
@@ -1,3 +1,5 @@
+class_name Vehicle
+
 extends Spatial
 
 
@@ -8,3 +10,13 @@ func _ready():
 			if x == 1 and y == 1:
 				continue
 			$GridMap.set_cell_item(x, y, 0, 0, 0)
+	_set_collision_box(Vector3(0, 0, 0), Vector3(2, 2, 0))
+
+
+func _set_collision_box(start: Vector3, end: Vector3) -> void:
+	start -= Vector3.ONE / 2
+	end += Vector3.ONE / 2
+	var center = (start + end) / 2
+	var extents = (end - start) / 2
+	$CollisionShape.transform.origin = center
+	$CollisionShape.shape.extents = extents
