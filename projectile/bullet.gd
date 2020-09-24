5a81525e87db02d0ddf52140bf517b899ed2166b
David Hoppenbrouwers <david@salt-inc.org>
2020-09-24 20:33:33 +0200

    Fix Bullet not damaging all Unit (non-Vehicle) objects

--- projectile/bullet.gd
+++ projectile/bullet.gd
@@ -18,11 +18,11 @@ func _physics_process(_delta):
 	var space = get_world().direct_space_state
 	var result = space.intersect_ray(previous_position, translation)
 	if len(result) > 0:
-		var vehicle := result.collider as Vehicle
-		if vehicle == null:
+		var unit := result.collider as Unit
+		if unit == null:
 			queue_free()
 		else:
-			damage = vehicle.projectile_hit(previous_position,
+			damage = unit.projectile_hit(previous_position,
 					translation - previous_position, damage)
 			if damage == 0:
 				queue_free()
