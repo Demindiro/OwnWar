fc0009a622ab9e5c20fa212d65c814c2a30a697c
David Hoppenbrouwers <david@salt-inc.org>
2020-09-24 20:31:40 +0200

    Fix Unit not despawning when health < 0

--- unit/unit.gd
+++ unit/unit.gd
@@ -6,9 +6,16 @@ export var max_health := 10
 export var team := 0
 
 onready var health := max_health
+onready var game_master = get_tree().get_current_scene()
 
 
 func projectile_hit(origin: Vector3, direction: Vector3, damage: int):
 	health -= damage
-	if health == 0:
+	if health <= 0:
 		queue_free()
+		return -health
+	return 0
+
+
+func get_actions():
+	return []
