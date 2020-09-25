56b1c1dd227f6a46a1a811b5314a9aaaae561044
David Hoppenbrouwers <david@salt-inc.org>
2020-09-25 14:37:27 +0200

    Add tutorial campaign button to main menu

--- menu/main.gd
+++ menu/main.gd
@@ -1,30 +1,27 @@
-extends GameMaster
+extends Control
 
 
 const WAYPOINT_MIN_RADIUS = 100
 const WAYPOINT_MAX_RADIUS = 200
 
-# God fucking damn cyclic references should be allowed damnit
-#export(PackedScene) var designer
-#export(PackedScene) var test_map
-export(String, FILE, "*.tscn") var designer
-export(String, FILE, "*.tscn") var test_map
+export(NodePath) var campaign_dialog
 
 
 func _ready():
 	randomize()
-	$Vehicle.load_from_file("user://vehicles/apc.json")
+	$"../Vehicle".load_from_file("user://vehicles/apc.json")
 	call_deferred("_on_Timer_timeout")
 
 
 func _on_Timer_timeout():
 	var angle = randf() * PI * 2
 	var distance = rand_range(WAYPOINT_MIN_RADIUS, WAYPOINT_MAX_RADIUS)
-	$Vehicle.ai.waypoint = Vector3(distance * cos(angle), 0, distance * sin(angle))
+	$"../Vehicle".ai.waypoint = Vector3(distance * cos(angle), 0, distance * sin(angle))
 
 
 func _on_Campaign_pressed():
-	pass # Replace with function body.
+	var node = get_node(campaign_dialog)
+	node.visible = not node.visible
 
 
 func _on_RandomMap_pressed():
@@ -32,11 +29,11 @@ func _on_RandomMap_pressed():
 
 
 func _on_Designer_pressed():
-	Global.goto_scene(designer)
+	Global.goto_scene(Global.SCENE_DESIGNER)
 
 
 func _on_DesignerMap_pressed():
-	Global.goto_scene(test_map)
+	Global.goto_scene(Global.SCENE_DESIGNER_MAP)
 
 
 func _on_Settings_pressed():
