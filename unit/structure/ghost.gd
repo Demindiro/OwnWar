9cad85def1a341855c594312c76e02df42927094
David Hoppenbrouwers <david@salt-inc.org>
2020-09-29 21:16:35 +0200

    Add build function and make Ghost inherit Unit

--- unit/structure/ghost.gd
+++ unit/structure/ghost.gd
@@ -1,5 +1,5 @@
 class_name Ghost
-extends Spatial
+extends Unit
 
 
 signal built(structure)
@@ -7,9 +7,7 @@ signal built(structure)
 export(PackedScene) var structure
 export(Array) var init_arguments
 export(int) var build_cost = 0
-export(int) var team = 0
 var build_progress = 0
-onready var game_master = get_tree().get_current_scene()
 
 
 func add_build_progress(material):
@@ -20,7 +18,6 @@ func add_build_progress(material):
 		unit.global_transform = global_transform
 		game_master.add_unit(team, unit)
 		unit.callv("init", init_arguments)
-		emit_signal("built", unit)
-		queue_free()
+		destroy()
 		return build_cost - build_progress
 	return 0
