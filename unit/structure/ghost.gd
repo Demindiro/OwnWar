fd2c4959618788b9b9544958e08f30a3daa7da22
David Hoppenbrouwers <david@salt-inc.org>
2020-09-30 23:20:13 +0200

    Implement get_info for Ghost

--- unit/structure/ghost.gd
+++ unit/structure/ghost.gd
@@ -10,6 +10,11 @@ export(Vector3) var spawn_offset = Vector3.ZERO
 var build_progress = 0
 
 
+func get_info():
+	var info = .get_info()
+	info["Progress"] = "%d / %d" % [build_progress, cost]
+	return info
+
 func add_build_progress(material):
 	build_progress += material
 	if build_progress >= cost:
