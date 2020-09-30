a57954c09a328b838630533fbcbe30a77bc44c49
David Hoppenbrouwers <david@salt-inc.org>
2020-09-30 15:27:50 +0200

    Increase health of StoragePod and set cost of SpawnPlatform

--- unit/structure/ghost.gd
+++ unit/structure/ghost.gd
@@ -6,14 +6,13 @@ signal built(structure)
 
 export(PackedScene) var structure
 export(Array) var init_arguments
-export(int) var build_cost = 0
 export(Vector3) var spawn_offset = Vector3.ZERO
 var build_progress = 0
 
 
 func add_build_progress(material):
 	build_progress += material
-	if build_progress >= build_cost:
+	if build_progress >= cost:
 		var unit = structure.instance()
 		unit.team = team
 		unit.global_transform = global_transform
@@ -22,5 +21,5 @@ func add_build_progress(material):
 		if unit.has_method("init") or init_arguments != []:
 			unit.callv("init", init_arguments)
 		destroy()
-		return build_cost - build_progress
+		return cost - build_progress
 	return 0
