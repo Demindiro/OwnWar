b3a992003fe6a1e403edeb927be82cc0afb05db1
David Hoppenbrouwers <david@salt-inc.org>
2020-10-03 16:08:39 +0200

    Add square corner generator without duplicates and sharp points

---
+++ block/chassis/variant/no_dup/square_corner.gd
@@ -0,0 +1,34 @@
+extends "res://block/chassis/variant/variant.gd"
+
+
+func _init():
+	name = "square_corner"
+	._set_generator()
+	._set_indices_count(5)
+
+
+func start(segments: int, scale: Vector3, offset: Vector3):
+	.start(segments, scale, offset)
+	step()
+
+
+func step():
+	.step()
+	while fractions[0] < fractions[1] or fractions[1] < fractions[2] \
+		or fractions[0] < fractions[3] or fractions[2] < fractions[4]:
+		if finished:
+			return
+		.step()
+		
+	var x = Vector3(fractions[0], 0, 0)
+	var y = Vector3(0, fractions[1], 0)
+	var z = Vector3(0, 0, fractions[2])
+	var u = Vector3(fractions[3], 0, fractions[4])
+	
+	result = [x, y, z, u]
+
+
+func get_mesh(data: Array):
+	assert(len(data) == 4)
+	return mesh_generator.generate(Transform.IDENTITY, data[0], data[1], data[2],
+			data[3])
