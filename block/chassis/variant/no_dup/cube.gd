64d95ee17db3b04da2c773a79b2b5ac2727f0922
David Hoppenbrouwers <david@salt-inc.org>
2020-10-03 17:03:00 +0200

    Add cube generator without duplicates and sharp points

---
+++ block/chassis/variant/no_dup/cube.gd
@@ -0,0 +1,32 @@
+extends "res://block/chassis/variant/complex/cube_b.gd"
+
+
+
+func step():
+	.step()
+	while not _is_valid():
+		.step()
+		if finished:
+			return
+
+
+func _is_valid():
+	var x = result[0]
+	var y = result[1]
+	var z = result[2]
+	var u = result[3]
+	var v = result[4]
+	var w = result[5]
+	var a = result[6]
+	# Dedup
+	if x.x < y.y or y.y < z.z:
+		return false
+	# No pointiness
+	if (y.y < u.y or z.z < u.z) or (x.x < v.x or z.z < v.z) or (x.x < w.x or y.y < w.y):
+		return false
+	var a_x = min(v.x, w.x)
+	var a_y = min(u.y, w.y)
+	var a_z = min(u.z, v.z)
+	if a_x < a.x or a_y < a.y or a_z < a.z:
+		return false
+	return true
