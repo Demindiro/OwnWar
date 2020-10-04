fb227a2876ac472f95b79a0ac591ff26a7171207
David Hoppenbrouwers <david@salt-inc.org>
2020-10-04 11:15:31 +0200

    Remove dupes of inverse corner

--- block/chassis/variant/no_dup/inverse_corner.gd
+++ block/chassis/variant/no_dup/inverse_corner.gd
@@ -1,6 +1,15 @@
 extends "res://block/chassis/variant/complex/inverse_corner.gd"
 
 
+var _blacklist = [
+		PoolIntArray([2, 2, 2, 2, 2, 2, 1, 2, 2]),
+		PoolIntArray([2, 2, 2, 2, 2, 2, 2, 1, 2]),
+		PoolIntArray([2, 2, 2, 2, 2, 2, 2, 2, 1]),
+		PoolIntArray([2, 2, 1, 2, 1, 1, 1, 2, 2]),
+		PoolIntArray([2, 2, 2, 2, 2, 2, 1, 2, 1]),
+	]
+
+
 func step():
 	.step()
 	while not _is_valid():
@@ -22,4 +31,16 @@ func _is_valid():
 	# No pointiness
 	if (y.y < u.y or z.z < u.z) or (x.x < v.x or z.z < v.z) or (x.x < w.x or y.y < w.y):
 		return false
-	return true
+	# No mirror
+	if u.y < u.z or v.x < v.z or w.y < w.z:
+		return false
+	# No vertices on diagonal
+	if (x - w).cross(y - x).length_squared() < 1e-5 or \
+			(x - v).cross(z - v).length_squared() < 1e-5 or \
+			(y - u).cross(z - u).length_squared() < 1e-5:
+		return false
+	return not _is_blacklisted()
+
+
+func _is_blacklisted():
+	return indice_generator.indices in _blacklist
