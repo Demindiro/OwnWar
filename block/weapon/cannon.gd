cf9d2a1dba62a344a5364855ba842019e928d7ff
David Hoppenbrouwers <david@salt-inc.org>
2020-10-07 15:57:25 +0200

    Add 80mm cannon

---
+++ block/weapon/cannon.gd
@@ -0,0 +1,32 @@
+extends RigidBody
+
+
+var _voxel_body: VoxelBody
+var _desired_direction := Vector3.FORWARD
+
+
+func _physics_process(_delta):
+	var other_forward = _voxel_body.global_transform.basis.z
+	var self_normal = global_transform.basis.x
+	var t = -self_normal.dot(other_forward) / self_normal.length_squared()
+	var projected_other_forward = (other_forward + t * self_normal).normalized()
+	var abs_desired_direction = global_transform.basis * _desired_direction
+	var error = 1.0 - projected_other_forward.dot(abs_desired_direction)
+	var direction = -sign(projected_other_forward \
+			.cross(abs_desired_direction) \
+			.dot(self_normal))
+	if abs(direction) < 1e-5:
+		direction = 1.0
+	var turn_rate = 0 if error < 1e-4 else direction
+	$Generic6DOFJoint.set("angular_motor_x/target_velocity", turn_rate)
+
+
+func init(_coordinate, _block_data, _rotation, voxel_body, _vehicle):
+	set_as_toplevel(true)
+	$Generic6DOFJoint.set("nodes/node_b", $Generic6DOFJoint.get_path_to(voxel_body))
+	_voxel_body = voxel_body
+	set_angle(PI / 8)
+
+
+func set_angle(angle):
+	_desired_direction = Vector3.BACK.rotated(Vector3.RIGHT, angle)
