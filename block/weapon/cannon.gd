f2aa118afa7de5aac398d040bc108e9ba6b720a1
David Hoppenbrouwers <david@salt-inc.org>
2020-10-08 18:26:01 +0200

    Fix twitching cannon

--- block/weapon/cannon.gd
+++ block/weapon/cannon.gd
@@ -23,7 +23,7 @@ func _physics_process(_delta):
 	var direction = -projected_other_forward \
 			.cross(abs_desired_direction) \
 			.dot(self_normal)
-	if abs(direction) < 1e-5:
+	if error > 1e-2 and abs(direction) < 1e-5:
 		direction = 1.0
 	var turn_rate = 0 if error < 1e-10 else direction * PI * 10
 	turn_rate = clamp(turn_rate, -PI / 2, PI / 2)
