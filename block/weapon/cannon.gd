d42af1cb87b803d5bc42e69c028775ec55824746
David Hoppenbrouwers <david@salt-inc.org>
2020-10-08 15:22:36 +0200

    Account for gravity in aim_at

--- block/weapon/cannon.gd
+++ block/weapon/cannon.gd
@@ -2,6 +2,7 @@ extends RigidBody
 class_name Cannon
 
 
+const GRAVITY = 9.8
 export var projectile: PackedScene
 export var reload_time := 5.0
 export var projectile_velocity := 1000.0
@@ -28,6 +29,20 @@ func _physics_process(_delta):
 	$Generic6DOFJoint.set("angular_motor_x/target_velocity", turn_rate)
 
 
+func _process(delta):
+	var debug = get_tree().current_scene.find_node("Debug")
+	var projectile_position = $ProjectileSpawn.global_transform.origin
+	var projectile_velocity_v = $ProjectileSpawn.global_transform.basis.z * projectile_velocity
+	debug.begin(Mesh.PRIMITIVE_LINE_STRIP)
+	debug.set_color(Color.lightgreen)
+	for i in range(int(20.0 / 0.1)):
+		debug.add_vertex(projectile_position)
+		projectile_velocity_v.y -= GRAVITY * 0.1 / 2
+		projectile_position += projectile_velocity_v * 0.1
+		projectile_velocity_v.y -= GRAVITY * 0.1 / 2
+	debug.end()
+
+
 func init(_coordinate, _block_data, _rotation, voxel_body, _vehicle):
 	_rel_offset = translation
 	print(_rel_offset)
@@ -38,10 +53,26 @@ func init(_coordinate, _block_data, _rotation, voxel_body, _vehicle):
 
 func aim_at(position: Vector3, _velocity := Vector3.ZERO):
 	var rel_pos = _voxel_body.to_local(position) - _rel_offset
-	var self_normal = Vector3.RIGHT
-	var t = -self_normal.dot(rel_pos) / self_normal.length_squared()
-	_desired_direction = (rel_pos + t * self_normal).normalized()
-	_desired_direction.y = -_desired_direction.y
+#	var self_normal = Vector3.RIGHT
+#	var t = -self_normal.dot(rel_pos) / self_normal.length_squared()
+#	_desired_direction = (rel_pos + t * self_normal).normalized()
+#	_desired_direction.y = -_desired_direction.y
+
+	var distance_xz = Vector2(rel_pos.x, rel_pos.z).length()
+	var distance_y = rel_pos.y
+
+	var normal_xz = rel_pos
+	normal_xz.y = 0
+	normal_xz = normal_xz.normalized()
+	
+	var x = distance_xz
+	var y = distance_y
+	var v2 = projectile_velocity * projectile_velocity
+	var g = GRAVITY
+	
+	var f = v2 * v2 - g * (g * x * x + 2 * y * v2)
+	if f >= 0:
+		set_angle(atan2(v2 - sqrt(f), g * x) if f >= 0 else 0)
 
 
 func fire():
