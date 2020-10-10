5d1b1c04563a4f5cda105233bcc83fd3c43d9c7b
David Hoppenbrouwers <david@salt-inc.org>
2020-10-10 10:30:30 +0200

    Use actual shell objects for shell storage

--- block/weapon/ammo_rack.gd
+++ block/weapon/ammo_rack.gd
@@ -2,7 +2,11 @@ extends Node
 
 
 export var max_shells := 4
-var shells := 0
+var shells := []
+
+
+func _enter_tree():
+	shells
 
 
 func init(_coordinate, _block_data, _rotation, _voxel_body, vehicle):
@@ -13,45 +17,47 @@ func init(_coordinate, _block_data, _rotation, _voxel_body, vehicle):
 	vehicle.add_info_function(self, "_static_get_info_shells", "Shells")
 
 
-func put_shell():
-	if shells < max_shells:
-		shells += 1
-		return 0
-	return 1
+func put_shell(shell):
+	if len(shells) < max_shells:
+		shells.append(shell)
+		return null
+	return shell
 
 
 func take_shell():
-	if shells > 0:
-		shells -= 1
-		return 1
-	return 0
+	if len(shells) > 0:
+		return shells.pop_back()
+	return null
 
 
-static func _static_put_shell(blocks, _arguments):
+static func _static_put_shell(blocks, arguments):
+	var shell = arguments[0]
 	for block in blocks:
-		if not block.put_shell():
-			return 0
-	return 1
+		shell = block.put_shell(shell)
+		if shell == null:
+			break
+	return shell
 
 
 static func _static_take_shell(blocks, _arguments):
 	for block in blocks:
-		if block.take_shell():
-			return 1
-	return 0
+		var shell = block.take_shell()
+		if shell != null:
+			return shell
+	return null
 
 
 static func _static_get_shell_space(blocks, _arguments):
 	var shell_space = 0
 	for block in blocks:
-		shell_space += block.max_shells - block.shells
+		shell_space += block.max_shells - len(block.shells)
 	return shell_space
 
 
 static func _static_get_shell_count(blocks, _arguments):
 	var shell_count = 0
 	for block in blocks:
-		shell_count += block.shells
+		shell_count += len(block.shells)
 	return shell_count
 
 
@@ -60,5 +66,5 @@ static func _static_get_info_shells(blocks):
 	var total_shells = 0
 	for block in blocks:
 		max_total_shells += block.max_shells
-		total_shells += block.shells
+		total_shells += len(block.shells)
 	return "%d / %d" % [total_shells, max_total_shells]
