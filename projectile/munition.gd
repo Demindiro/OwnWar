9a469b1a288e77b7c44d9614a83e9ad96290d0b3
David Hoppenbrouwers <david@salt-inc.org>
2020-10-10 11:23:51 +0200

    Make munition factory create actual munition objects

--- projectile/munition.gd
+++ projectile/munition.gd
@@ -6,3 +6,4 @@ export var shell: PackedScene
 export var count := 1
 export var max_shells := 1
 export var mesh: Mesh
+export var cost := 3
--- unit/structure/munitions_factory.gd
+++ unit/structure/munitions_factory.gd
@@ -2,35 +2,38 @@ extends Unit
 
 
 export var max_material := 10
-export var max_shells := 20
-export var material_per_shell := 3
-export var time_between_shells := 1.0
+export var max_munition := 20
+export var material_per_munition := 3
+export var time_between_munitions := 1.0
+#export(Array, Munition) var munition_types := []
+export(Array, Resource) var munition_types := []
 var material := 0
-var shells := 0
-var _producing_shell := false
-var _time_until_shell_produced := 0.0
+var munition := []
+var _producing_munition := false
+var _time_until_munition_produced := 0.0
 
 
 func _physics_process(delta):
-	if _producing_shell:
-		if _time_until_shell_produced >= time_between_shells:
-			if shells < max_shells:
-				shells += 1
-				_visualize_shells()
-				_producing_shell = false
-				_time_until_shell_produced -= time_between_shells
+	if _producing_munition:
+		if _time_until_munition_produced >= time_between_munitions:
+			if len(munition) < max_munition:
+				var new_munition = munition_types[0].duplicate()
+				munition.append(new_munition)
+				_visualize_munitions()
+				_producing_munition = false
+				_time_until_munition_produced -= time_between_munitions
 		else:
-			_time_until_shell_produced += delta
+			_time_until_munition_produced += delta
 	else:
-		if material_per_shell <= material:
-			material -= material_per_shell
-			_producing_shell = true
+		if material_per_munition <= material:
+			material -= material_per_munition
+			_producing_munition = true
 
 
 func get_info():
 	var info = .get_info()
 	info["Material"] = "%d / %d" % [material, max_material]
-	info["Shells"] = "%d / %d" % [shells, max_shells]
+	info["Munition"] = "%d / %d" % [len(munition), max_munition]
 	return info
 
 
@@ -43,21 +46,21 @@ func put_material(p_material: int) -> int:
 	return remainder
 
 
-func take_shell():
-	if shells > 0:
-		shells -= 1
-		_visualize_shells()
-		return true
-	return false
+func take_munition():
+	if len(munition) > 0:
+		var taken_munition = munition.pop_front()
+		_visualize_munitions()
+		return taken_munition
+	return null
 
 
-func get_shell_count():
-	return shells
+func get_munition_count():
+	return len(munition)
 
 
-func _visualize_shells():
-	$MultiMeshInstance.multimesh.instance_count = shells
-	for i in range(shells):
-		var shell_transform := Transform2D(Vector2.UP, Vector2.RIGHT,
+func _visualize_munitions():
+	$MultiMeshInstance.multimesh.instance_count = len(munition)
+	for i in range(len(munition)):
+		var munition_transform := Transform2D(Vector2.UP, Vector2.RIGHT,
 			Vector2(i % 5, i / 5) / 3)
-		$MultiMeshInstance.multimesh.set_instance_transform_2d(i, shell_transform)
+		$MultiMeshInstance.multimesh.set_instance_transform_2d(i, munition_transform)
