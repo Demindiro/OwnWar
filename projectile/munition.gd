13be1c49c703d185855ac698c5628b87b61380d4
David Hoppenbrouwers <david@salt-inc.org>
2020-10-11 17:11:15 +0200

    Add human name to sheels and update 35mm model/stats

--- projectile/munition.gd
+++ projectile/munition.gd
@@ -2,8 +2,13 @@ class_name Munition
 extends Resource
 
 
+export var human_name := ""
 export var shell: PackedScene
 export var count := 1
 export var max_shells := 1
 export var mesh: Mesh
 export var cost := 3
+
+
+func _to_string():
+	return human_name
