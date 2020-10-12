584d30d4f67b94c0761516755db5ce2b51795eb5
David Hoppenbrouwers <david@salt-inc.org>
2020-10-12 16:05:43 +0200

    Fix ammo rack being added before gauge is set

--- block/weapon/ammo_rack.gd
+++ block/weapon/ammo_rack.gd
@@ -7,6 +7,6 @@ var gauge := 0
 
 func init(_coordinate, _block_data, _rotation, _voxel_body, vehicle, meta):
 	var manager = vehicle.get_manager("weapon", preload("res://block/weapon/weapon_manager.gd"))
-	manager.add_ammo_rack(self)
 	if meta != null:
 		gauge = meta["gauge_filter"]
+	manager.add_ammo_rack(self)
