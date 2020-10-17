f2e8c074f06a8110aa853977631d028d312b6ab6
David Hoppenbrouwers <david@salt-inc.org>
2020-10-17 10:17:14 +0200

    Implement *_matter* functions for refinery

--- unit/structure/refinery/refinery.gd
+++ unit/structure/refinery/refinery.gd
@@ -2,7 +2,7 @@ extends Unit
 
 
 const MAX_MATERIAL := 50
-export var max_fuel := 1000
+const MAX_FUEL := 1000
 export var material := 0
 export var fuel := 0
 var _time_until_fuel_produced := 0.0
@@ -14,7 +14,7 @@ onready var _fuel_id: int = Matter.name_to_id["fuel"]
 func _physics_process(delta):
 	if _producing:
 		_time_until_fuel_produced += delta
-		if _time_until_fuel_produced >= 1.0 and fuel < max_fuel:
+		if _time_until_fuel_produced >= 1.0 and fuel < MAX_FUEL:
 			fuel += 1
 			_time_until_fuel_produced = 0.0
 			_producing = false
@@ -28,7 +28,7 @@ func _physics_process(delta):
 func get_info():
 	var info = .get_info()
 	info["Material"] = "%d / %d" % [material, MAX_MATERIAL]
-	info["Fuel"] = "%d / %d" % [fuel, max_fuel]
+	info["Fuel"] = "%d / %d" % [fuel, MAX_FUEL]
 	return info
 
 
@@ -38,37 +38,63 @@ func request_info(info: String):
 	return .request_info(info)
 
 
-func put_material(amount):
-	material += amount
-	if material > MAX_MATERIAL:
-		var remainder = material - MAX_MATERIAL
-		material = MAX_MATERIAL
-		send_message("need_material", MAX_MATERIAL - material)
-		return remainder
-	send_message("need_material", MAX_MATERIAL - material)
+func get_matter_count(id: int):
+	if id == _material_id:
+		return material
+	elif id == _fuel_id:
+		return fuel
 	return 0
 
 
-func take_material(amount):
-	material -= amount
-	if material < 0:
-		amount += material
-		material = 0
+func get_matter_space(id: int) -> int:
+	if id == _material_id:
+		return MAX_MATERIAL - material
+	elif id == _fuel_id:
+		return MAX_FUEL - fuel
+	return 0
+
+
+func put_matter(id: int, amount: int) -> int:
+	if id == _material_id:
+		material += amount
+		if material > MAX_MATERIAL:
+			var remainder = material - MAX_MATERIAL
+			material = MAX_MATERIAL
+			send_message("need_material", MAX_MATERIAL - material)
+			return remainder
 		send_message("need_material", MAX_MATERIAL - material)
+		return 0
 	return amount
+
+
+func take_matter(id: int, amount: int) -> int:
+	if id == _material_id:
+		material -= amount
+		if material < 0:
+			amount += material
+			material = 0
+			send_message("need_material", MAX_MATERIAL - material)
+		return amount
+	elif id == _fuel_id:
+		fuel -= amount
+		if fuel < 0:
+			amount += fuel
+			fuel = 0
+		return amount
+	return 0
+
+
+func put_material(amount):
+	return put_matter(_material_id, amount)
+
+
+func take_material(amount):
+	return take_matter(_material_id, amount)
 
 
 func take_fuel(amount):
-	fuel -= amount
-	if fuel < 0:
-		amount += fuel
-		fuel = 0
-	return amount
-
-
-func get_fuel_count():
-	return fuel
+	return take_matter(_fuel_id, amount)
 
 
 func get_material_space():
-	return MAX_MATERIAL - material
+	return get_matter_space(_material_id)
