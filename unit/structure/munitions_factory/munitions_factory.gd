c9971ee3e5a47eb020d1bfec89367041d45e0758
David Hoppenbrouwers <david@salt-inc.org>
2020-10-17 12:56:52 +0200

    Implement *_matter* functions for munition factory

--- unit/structure/munitions_factory/munitions_factory.gd
+++ unit/structure/munitions_factory/munitions_factory.gd
@@ -1,17 +1,18 @@
 extends Unit
 
 
-export var max_material := 10
-export var max_munition := 20
-export var material_per_munition := 3
-export var time_between_munitions := 1.0
 #export(Array, Munition) var munition_types := []
 export(Array, Resource) var munition_types := []
-var material := 0
-var munition := []
+const _MAX_MATERIAL := 30
+const _MAX_MUNITION_VOLUME := 3000
+var _material := 0
+var _munition := {}
+var _munition_volume := 0
 var _current_munition_type: Munition
 var _producing_munition := false
+var _time_between_munitions := 1.0
 var _time_until_munition_produced := 0.0
+onready var _material_id = Matter.name_to_id["material"]
 
 
 func _ready():
@@ -20,19 +21,21 @@ func _ready():
 
 func _physics_process(delta):
 	if _producing_munition:
-		if _time_until_munition_produced >= time_between_munitions:
-			if len(munition) < max_munition:
-				var new_munition = _current_munition_type.duplicate()
-				munition.append(new_munition)
+		if _time_until_munition_produced >= _time_between_munitions:
+			var id = Matter.name_to_id[_current_munition_type.human_name]
+			var volume = Matter.matter_volume[id]
+			if _munition_volume + volume < _MAX_MUNITION_VOLUME:
+				_munition[id] = _munition.get(id, 0) + 1
+				_munition_volume += volume
 				_visualize_munitions()
 				_producing_munition = false
-				_time_until_munition_produced -= time_between_munitions
+				_time_until_munition_produced -= _time_between_munitions
 		else:
 			_time_until_munition_produced += delta
 	else:
-		if material_per_munition <= material:
-			material -= material_per_munition
-			send_message("need_material", max_material - material)
+		if _current_munition_type.cost <= _material:
+			_material -= _current_munition_type.cost
+			send_message("need_material", _MAX_MATERIAL - _material)
 			_producing_munition = true
 
 
@@ -50,46 +53,86 @@ func get_actions():
 
 func get_info():
 	var info = .get_info()
-	info["Material"] = "%d / %d" % [material, max_material]
-	info["Munition"] = "%d / %d" % [len(munition), max_munition]
+	info["Material"] = "%d / %d" % [_material, _MAX_MATERIAL]
 	info["Producing"] = str(_current_munition_type)
+	info["Volume"] = "%d / %d" % [_munition_volume, _MAX_MUNITION_VOLUME]
+	for m in _munition:
+		info[Matter.matter_name[m]] = _munition[m]
 	return info
 
 
 func request_info(info: String):
 	if info == "need_material":
-		return max_material - material
+		return get_matter_space(_material_id)
 	return .request_info(info)
 
 
+func get_matter_count(id: int) -> int:
+	if id == _material_id:
+		return _material
+	return _munition.get(id, 0)
+
+
+func get_matter_space(id: int) -> int:
+	if id == _material_id:
+		return _MAX_MATERIAL - _material
+	elif Munition.is_munition(id):
+		var v := Matter.matter_volume[id]
+		return (_MAX_MUNITION_VOLUME - _munition_volume) / v
+	return 0
+
+
+func put_matter(id: int, amount: int) -> int:
+	if id == _material_id:
+		var remainder = 0
+		_material += amount
+		if _material > _MAX_MATERIAL:
+			remainder = _material - _MAX_MATERIAL
+			_material = _MAX_MATERIAL
+		send_message("need_material", _MAX_MATERIAL - _material)
+		return remainder
+	return amount
+
+
+func take_matter(id: int, amount: int) -> int:
+	if id in _munition:
+		if _munition[id] >= amount:
+			_munition[id] -= amount
+		else:
+			amount = _munition[id]
+			_munition.erase(id)
+		_munition_volume -= amount * Matter.matter_volume[id]
+		_visualize_munitions()
+		return amount
+	return 0
+
+
 func put_material(p_material: int) -> int:
-	var remainder = 0
-	material += p_material
-	if material > max_material:
-		remainder = material - max_material
-		material = max_material
-	send_message("need_material", max_material - material)
-	return remainder
+	return put_matter(_material_id, p_material)
 
 
 func take_munition():
-	if len(munition) > 0:
-		var taken_munition = munition.pop_front()
-		_visualize_munitions()
-		return taken_munition
+	for id in _munition:
+		var amount = take_matter(id, 1)
+		if amount > 0:
+			_visualize_munitions()
+			return RegisterMunition.id_to_munitions[id].duplicate()
 	return null
 
 
 func get_munition_count():
-	return len(munition)
+	var count := 0
+	for c in _munition.values():
+		count += c
+	return count
 
 
-func get_munition_space():
-	return max_munition - len(munition)
+#func get_munition_space():
+#	return max_munition - len(munition)
 
 
 func get_material_space():
-	return max_material - material
+	return get_matter_space(_material_id)
 
 
 func set_munition_type(flags, munition_type):
@@ -97,9 +140,10 @@ func set_munition_type(flags, munition_type):
 
 
 func _visualize_munitions():
+	var id = Matter.name_to_id[_current_munition_type.human_name]
 	$MultiMeshInstance.multimesh.mesh = _current_munition_type.mesh
-	$MultiMeshInstance.multimesh.instance_count = len(munition)
-	for i in range(len(munition)):
+	$MultiMeshInstance.multimesh.instance_count = _munition[id]
+	for i in range(_munition[id]):
 		var munition_transform := Transform2D(Vector2.UP, Vector2.RIGHT,
 			Vector2(i % 5, i / 5) / 3)
 		$MultiMeshInstance.multimesh.set_instance_transform_2d(i, munition_transform)
