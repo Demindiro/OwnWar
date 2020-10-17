a33a7e78f4fcc21a05ba9d8085bf51b86e6c5d45
David Hoppenbrouwers <david@salt-inc.org>
2020-10-17 02:16:09 +0200

    Add Matter singleton

---
+++ matter.gd
@@ -0,0 +1,23 @@
+class_name Matter
+extends Node
+# Singleton to track all types of "matter" (AKA Resources, but that name is
+# already taken)
+
+
+var matter_name := PoolStringArray()
+var matter_volume := PoolIntArray()
+var name_to_id := {}
+
+
+# Add matter. 1 unit of volume is considered to be 1 mm^3, so 1 dm^3 (liters) is
+# equivalent to 1_000_000 units and 1 m^3 is equivalent to 1_000_000_000 units
+#
+# The largest volume possible is thus 2^63 / 10 ^ 9 == 9223372036 m^3 or 10 km^3
+# For comparison, that's roughly 3.6 billion Schwerer Gustav shells each fitted
+# in a rectangular box.
+func add_matter(p_name: String, volume: int) -> void:
+	assert(not p_name in name_to_id)
+	assert(len(matter_name) == len(matter_volume))
+	matter_name.append(p_name)
+	matter_volume.append(volume)
+	name_to_id[p_name] = len(matter_name)
