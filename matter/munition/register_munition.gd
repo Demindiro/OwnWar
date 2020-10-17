beb8c598256c163515a903dad8690ca6bbf4ca6b
David Hoppenbrouwers <david@salt-inc.org>
2020-10-17 02:49:54 +0200

    Make RegisterMunition an autoload, add conversions for ammo

--- matter/munition/register_munition.gd
+++ matter/munition/register_munition.gd
@@ -1,11 +1,17 @@
-class_name RegisterMunition
 extends Node
 
 
 var id_to_munitions := {}
 
 
-func _ready():
+func _ready(deferred := false):
+	var material_id: int = Matter.name_to_id.get("material", -1)
+	if material_id < 0:
+		if deferred:
+			Global.error("Matter 'material' not found!")
+			return
+		else:
+			call_deferred("_ready", true)
 	for m in [
 			preload("160mm/shell_160mm.tres"),
 			preload("35mm/shell_35mm.tres"),
@@ -15,3 +21,4 @@ func _ready():
 		var volume: float = m.gauge * m.gauge * (m.gauge * 3.0)
 		var id := Matter.add_matter(m.human_name, m.gauge)
 		id_to_munitions[id] = m
+		Matter.add_conversion(id, material_id, m.cost)
