0952cfa38e836f264f316fdf7e50368b394cbeb7
David Hoppenbrouwers <david@salt-inc.org>
2020-10-17 02:37:30 +0200

    Remove count and max_shells from Munition

--- matter/munition/munition.gd
+++ matter/munition/munition.gd
@@ -4,8 +4,6 @@ extends Resource
 
 export var human_name := ""
 export var shell: PackedScene
-export var count := 1
-export var max_shells := 1
 export var mesh: Mesh
 export var cost := 3
 export var gauge := -1
