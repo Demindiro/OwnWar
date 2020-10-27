b9768fc53ad47f7aed4d9dfdc29579f2d4658ea3
David Hoppenbrouwers <david@salt-inc.org>
2020-10-27 17:18:50 +0100

    Replace campaign list with map list

---
+++ core/menu/main_menu/maps.gd
@@ -0,0 +1,12 @@
+extends "../dialog/independent_panel.gd"
+
+
+export var button_template: PackedScene
+
+
+func _ready():
+	for map in Maps.MAPS:
+		var button: Button = button_template.instance()
+		button.text = map
+		button.connect("pressed", Global, "goto_scene", [Maps.MAPS[map]])
+		add_child(button)
