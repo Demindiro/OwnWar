adfd7028d470168eedab31972f30f4e29f21ae7d
David Hoppenbrouwers <david@salt-inc.org>
2020-11-01 18:44:00 +0100

    Allow loading games from the saves UI

--- core/menu/main_menu/saves.gd
+++ core/menu/main_menu/saves.gd
@@ -5,11 +5,11 @@ extends "res://core/menu/dialog/independent_panel.gd"
 var _saves_hash := 0
 
 
-func _ready():
+func _ready() -> void:
 	_refresh_save_list()
 
 
-func _refresh_save_list():
+func _refresh_save_list() -> void:
 	var saves: Array = Util.iterate_dir_recursive("user://game_saves", "json")
 	var h := saves.hash()
 	if h != _saves_hash:
@@ -18,7 +18,12 @@ func _refresh_save_list():
 		for path in saves:
 			var button := Button.new()
 			button.text = path.get_file().get_basename()
+			button.connect("pressed", self, "_load_game", [path])
 			$VBoxContainer.add_child(button)
 		_saves_hash = h
 # warning-ignore:return_value_discarded
 	get_tree().create_timer(1.0, true).connect("timeout", self, "_refresh_save_list")
+
+
+func _load_game(path: String) -> void:
+	GameMaster.load_game(path)
