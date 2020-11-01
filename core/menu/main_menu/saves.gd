2cfcdd4df12a04d57d4c9be95542d2e83837f2c2
David Hoppenbrouwers <david@salt-inc.org>
2020-11-01 12:01:20 +0100

    Add game saves listing

---
+++ core/menu/main_menu/saves.gd
@@ -0,0 +1,24 @@
+tool
+extends "res://core/menu/dialog/independent_panel.gd"
+
+
+var _saves_hash := 0
+
+
+func _ready():
+	_refresh_save_list()
+
+
+func _refresh_save_list():
+	var saves: Array = Util.iterate_dir_recursive("user://game_saves", "json")
+	var h := saves.hash()
+	if h != _saves_hash:
+		print("Reloading saves list")
+		Util.free_children($VBoxContainer)
+		for path in saves:
+			var button := Button.new()
+			button.text = path.get_file().get_basename()
+			$VBoxContainer.add_child(button)
+		_saves_hash = h
+# warning-ignore:return_value_discarded
+	get_tree().create_timer(1.0, true).connect("timeout", self, "_refresh_save_list")
