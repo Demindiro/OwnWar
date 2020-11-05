d2a0411ae97efeea988bf52c7365d2e66e6a4e81
David Hoppenbrouwers <david@salt-inc.org>
2020-11-05 23:16:57 +0100

    Make subresources in cannons external

---
+++ plugins/cannon/160mm/collision_shape_accuracy_fix.gd
@@ -0,0 +1,11 @@
+# REEEEEEEEEEEEEEEEEEEEEe
+# https://github.com/godotengine/godot/issues/18251
+# Godot, come on, really? What is this trash UX?
+
+
+extends CollisionShape
+
+
+
+func _init():
+	shape.radius = 0.115
