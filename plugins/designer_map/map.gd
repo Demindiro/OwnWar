4565fa4ba2176a578673b929ba6524339330c05e
David Hoppenbrouwers <david@salt-inc.org>
2020-11-22 02:33:28 +0100

    Save UIDs of special structures in designer map

--- plugins/designer_map/map.gd
+++ plugins/designer_map/map.gd
@@ -1,23 +1,49 @@
 extends Node
 
 
-var game_master
-var material_id
+onready var game_master = GameMaster.get_game_master(self)
+onready var material_id = Matter.get_matter_id("material")
+
+var _player_spawn_platform_uid := -1
+var _enemy_spawn_platform_uid := -1
+var _player_storage_pod_uid := -1
+
+
+func _ready(deferred := false):
+	if deferred:
+		game_master.get_unit_by_uid(_player_storage_pod_uid) \
+				.put_matter(material_id, 500)
+	else:
+		_player_spawn_platform_uid = $"../Player/SpawnPlatform".uid
+		_enemy_spawn_platform_uid = $"../Enemy/SpawnPlatform".uid
+		_player_storage_pod_uid = $"../Player/StoragePod".uid
+		call_deferred("_ready", true)
 
 
 func _physics_process(_delta):
-	game_master.get_unit_by_uid(0).put_matter(material_id, 1 << 62)
-	game_master.get_unit_by_uid(241).put_matter(material_id, 1 << 62)
+	game_master.get_unit_by_uid(_player_spawn_platform_uid) \
+			.put_matter(material_id, 1 << 62)
+	game_master.get_unit_by_uid(_enemy_spawn_platform_uid) \
+			.put_matter(material_id, 1 << 62)
 
 
-func _on_FilthyHack_ready():
-	# I need something that is called after _enter_tree but just right before
-	# _ready. This works I guess...
-	game_master = GameMaster.get_game_master(self)
-	material_id = Matter.get_matter_id("material")
-	for child in get_children():
-		if child is Unit:
-			game_master.units[child.team].append(child)
-			child.uid = game_master.uid_counter
-			game_master.uid_counter += 241
-	game_master.get_unit_by_uid(964).put_matter(material_id, 500)
+func _on_Designer_save_game(data: Dictionary) -> void:
+	data["designer_map_data"] = {
+			"player_spawn_platform_uid": _player_spawn_platform_uid,
+			"enemy_spawn_platform_uid": _enemy_spawn_platform_uid,
+			"player_storage_pod_uid": _player_storage_pod_uid,
+		}
+
+
+func _on_Designer_load_game(data: Dictionary) -> void:
+	var d = data.get("designer_map_data")
+	if d != null:
+		_player_spawn_platform_uid = d["player_spawn_platform_uid"]
+		_enemy_spawn_platform_uid = d["player_spawn_platform_uid"]
+		_player_storage_pod_uid = d["player_spawn_platform_uid"]
+	else:
+		# Magic constants from before v0.14.1
+		_player_spawn_platform_uid = 0
+		_enemy_spawn_platform_uid = 241
+		_player_storage_pod_uid = 964
+
