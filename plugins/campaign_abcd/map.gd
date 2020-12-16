440a69040e9199a7143d5ee52389469b3706d1de
David Hoppenbrouwers <david@salt-inc.org>
2020-12-16 18:41:02 +0100

    Add default GUI to campaign ABCD

--- plugins/campaign_abcd/map.gd
+++ plugins/campaign_abcd/map.gd
@@ -1,1 +1,24 @@
-extends OwnWar.GameMaster
+extends OwnWar_GameMaster
+
+
+onready var _gui: Control = $GUI
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		_gui.visible = true
+		get_tree().paused = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_GUI_cancel():
+	_gui.visible = false
+	get_tree().paused = false
+
+
+func _on_GUI_restart():
+	Global.goto_scene(filename)
+
+
+func _on_GUI_exit():
+	OwnWar.goto_main_menu(get_tree())
