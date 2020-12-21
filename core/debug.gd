949cccf314531c96f3c5bc44780095762a2e0506
David Hoppenbrouwers <david@salt-inc.org>
2020-12-21 03:43:33 +0100

    Add draw_text to Debug

--- core/debug.gd
+++ core/debug.gd
@@ -1,6 +1,17 @@
 extends Spatial
 
 
+class Text3D:
+	var text: String
+	var position: Vector3
+	var color: Color
+
+	func _init(p_position: Vector3, p_text: String, p_color: Color) -> void:
+		text = p_text
+		position = p_position
+		color = p_color
+
+
 var _assert_drawing := false
 var _funcrefs := {}
 var _cam: Camera = null
@@ -10,6 +21,9 @@ var _vp_rect: Rect2
 var _im := ImmediateGeometry.new()
 var _point_mesh: ArrayMesh
 var _point_multimeshes := {}
+var _canvas_item := Control.new()
+var _canvas_item_text3d := []
+onready var _default_font := Control.new().get_font("font")
 
 
 func _init():
@@ -24,11 +38,18 @@ func _init():
 func _enter_tree():
 	_vp = get_tree().root
 	add_child(_im)
+	add_child(_canvas_item)
+	_canvas_item.anchor_bottom = 1
+	_canvas_item.anchor_right = 1
+	_canvas_item.focus_mode = Control.FOCUS_NONE
+	_canvas_item.focus_mode = Control.FOCUS_NONE
+	_canvas_item.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	var tr := get_tree()
 	var e := tr.connect("node_added", self, "_node_added")
 	assert(e == OK)
 	e = tr.connect("node_removed", self, "_node_removed")
 	assert(e == OK)
+	e = _canvas_item.connect("draw", self, "_draw_canvas_item")
 
 
 func _process(_delta):
@@ -38,6 +59,7 @@ func _process(_delta):
 		for v in _point_multimeshes.values():
 			var mm: MultiMesh = v
 			mm.visible_instance_count = 0
+		_canvas_item_text3d.clear()
 		# Get camera properties
 		_cam = _vp.get_camera()
 		if _cam == null:
@@ -51,6 +73,8 @@ func _process(_delta):
 			_funcrefs[node].call_func()
 		_assert_drawing = false
 		_im.end()
+		_canvas_item.update()
+	_canvas_item.visible = visible
 
 
 func draw_point(origin: Vector3, color := Color.white, radius := 1.0):
@@ -119,6 +143,11 @@ func draw_graph(points: PoolVector3Array, color := Color.white):
 		v_prev_vis = v_vis
 
 
+func draw_text(origin: Vector3, text: String, color := Color.white) -> void:
+	assert(_assert_drawing)
+	_canvas_item_text3d.append(Text3D.new(origin, text, color))
+
+
 func _node_added(node: Node) -> void:
 	assert(not node in _funcrefs)
 	if node.has_method("debug_draw"):
@@ -151,3 +180,14 @@ func _create_point_mesh():
 	arr[Mesh.ARRAY_VERTEX] = verts
 	_point_mesh = ArrayMesh.new()
 	_point_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINES, arr)
+
+
+func _draw_canvas_item():
+	for t3d in _canvas_item_text3d:
+		var text3d: Text3D = t3d
+		if _is_point_visible(text3d.position):
+			var pos := _cam.unproject_position(text3d.position)
+			var text := text3d.text.split('\n')
+			for t in text:
+				_canvas_item.draw_string(_default_font, pos, t, text3d.color)
+				pos += Vector2(0, 16.0)
