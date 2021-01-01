05036b42c04e13c1766d0a81a0a2c2725ec0cee3
David Hoppenbrouwers <david@salt-inc.org>
2021-01-01 18:27:07 +0100

    Move wheel plugin to blocks/wheels

--- core/global.gd
+++ core/global.gd
@@ -74,6 +74,7 @@ func _init():
 # warning-ignore:return_value_discarded
 	OwnWar.Matter.add_matter("fuel", 100_000)
 	preload("res://blocks/chassis/chassis.gd").load_blocks()
+	OwnWar.Block.add_block(preload("res://blocks/wheels/big-wheel/wheel.tres"))
 
 
 
