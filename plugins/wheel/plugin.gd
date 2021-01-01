03b3d558b3812dea5421d7582de77870b85add0d
David Hoppenbrouwers <david@salt-inc.org>
2021-01-01 18:00:44 +0100

    Remove power_manager from wheel dependencies

--- plugins/wheel/plugin.gd
+++ plugins/wheel/plugin.gd
@@ -5,7 +5,6 @@ const PLUGIN_VERSION := Vector3(0, 0, 1)
 const MIN_VERSION := Vector3(0, 12, 0)
 const PLUGIN_DEPENDENCIES := {
 		"movement_manager": Vector3(0, 0, 1),
-		"power_manager": Vector3(0, 0, 1),
 	}
 
 
