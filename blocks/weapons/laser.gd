900a13f2d2d6959e22e969e2aa92cc50b9d3b91f
David Hoppenbrouwers <david@salt-inc.org>
2021-01-02 04:32:12 +0100

    Interpolate cannon visuals, reduce cannon turn limits

--- blocks/weapons/laser.gd
+++ blocks/weapons/laser.gd
@@ -12,9 +12,26 @@ var _time_of_last_shot := 0.0
 var _aim_pos := Vector3()
 onready var _projectile_spawn: Spatial = $ProjectileSpawn
 onready var _joint: Generic6DOFJoint = get_node("Generic6DOFJoint")
+onready var _prev_transform := global_transform
+onready var _visual: Spatial = get_node("Visual")
+
+
+func _ready() -> void:
+	if OS.has_feature("Server"):
+		set_process(false)
+		_visual.free()
+	elif not OwnWar.is_in_designer(get_tree()):
+		_visual.set_as_toplevel(true)
+
+
+func _process(_delta: float) -> void:
+	var frac := Engine.get_physics_interpolation_fraction()
+	var trf := _prev_transform.interpolate_with(transform, frac)
+	_visual.transform = trf
 
 
 func _physics_process(delta: float) -> void:
+	_prev_transform = global_transform
 	var curr_dir := (_aim_pos - global_transform.origin).normalized()
 	var error := curr_dir.dot(global_transform.basis.z)
 	var side := -sign(curr_dir.dot(global_transform.basis.y))
