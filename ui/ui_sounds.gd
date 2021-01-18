a303ea8bbbffe1e4797476505963e341619bdd36
David Hoppenbrouwers <david@salt-inc.org>
2021-01-18 21:34:24 +0100

    Play sound when hovering and clicking buttons

---
+++ ui/ui_sounds.gd
@@ -0,0 +1,54 @@
+extends Node
+
+
+var audio_hover := AudioStreamPlayer.new()
+var audio_click := AudioStreamPlayer.new()
+
+
+func _enter_tree() -> void:
+	var e := get_tree().connect("node_added", self, "node_added")
+	assert(e == OK)
+	e = get_tree().connect("node_removed", self, "node_removed")
+	assert(e == OK)
+	audio_hover.stream = preload("res://addons/ui/397599__nightflame__menu-fx-02.wav")
+	audio_hover.bus = "UI"
+	add_child(audio_hover)
+	audio_click.stream = preload("res://addons/ui/397599__nightflame__menu-fx-02.wav")
+	audio_click.bus = "UI"
+	audio_click.volume_db *= 5
+	audio_click.pitch_scale *= 1.25
+	add_child(audio_click)
+
+
+func node_added(node: Node) -> void:
+	if node is BaseButton:
+		var e := node.connect("mouse_entered", self, "mouse_entered", [node])
+		assert(e == OK)
+		e = node.connect("mouse_exited", self, "mouse_exited", [node])
+		assert(e == OK)
+		e = node.connect("pressed", audio_click, "play")
+		assert(e == OK)
+		# Recursing is necessary because https://github.com/godotengine/godot/issues/16854
+		for n in Util.get_children_recursive(node):
+			if n is Control:
+				e = n.connect("mouse_entered", self, "mouse_entered", [node])
+				assert(e == OK)
+				e = n.connect("mouse_exited", self, "mouse_exited", [node])
+				assert(e == OK)
+		node.set_meta("ui_audio_mouse_entered", false)
+
+
+func node_removed(node: Node) -> void:
+	# TODO is it safe to assume the node has been deleted?
+	pass
+
+
+func mouse_entered(node: BaseButton) -> void:
+	assert(node != null)
+	if not node.get_meta("ui_audio_mouse_entered"):
+		audio_hover.play()
+	node.call_deferred("set_meta", "ui_audio_mouse_entered", true)
+
+
+func mouse_exited(node: BaseButton) -> void:
+	node.call_deferred("set_meta", "ui_audio_mouse_entered", false)
