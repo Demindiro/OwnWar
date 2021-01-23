ea37261a7b1147247ffa0ac45c099d423dd843ac
David Hoppenbrouwers <david@salt-inc.org>
2021-01-23 04:27:44 +0100

    Bump version to 0.17.1, split server install script

---
+++ install-lobby.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+scp bin/ownwar.pck ownwar@lv0:ownwar.pck
+scp lobby/target/release/own-war-lobby ownwar@lv0:own-war-lobby
--- install-server.sh
+++ install-server.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 scp bin/ownwar.pck ownwar@lv0:ownwar.pck
-scp lobby/target/release/own-war-lobby ownwar@lv0:own-war-lobby
