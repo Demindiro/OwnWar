7da47eabd90f81476839e513555176e776853731
David Hoppenbrouwers <david@salt-inc.org>
2021-03-21 23:18:23 +0100

    Update server install script

--- install-server.sh
+++ install-server.sh
@@ -2,7 +2,9 @@
 
 for SERVER in lu0 lv0; do
 	echo Installing on $SERVER
-	scp bin/linux/libhterrain.so "ownwar@$SERVER:libhterrain.so"
-	scp bin/linux/libownwar.so "ownwar@$SERVER:libownwar.so"
-	scp bin/linux/ownwar.pck "ownwar@$SERVER:ownwar.pck"
+	cd bin/linux
+	for file in *.so; do
+		scp "$file" "ownwar@$SERVER:$file"
+	done
+	scp ownwar.pck "ownwar@$SERVER:ownwar.pck"
 done
