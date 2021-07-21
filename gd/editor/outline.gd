aa930a9de787036571b88e5a4d7bf8b7eeb76a63
David Hoppenbrouwers <david@salt-inc.org>
2021-07-21 20:27:09 +0200

    Fix broken visuals for disconnected blocks.

--- gd/editor/outline.gd
+++ gd/editor/outline.gd
@@ -6,12 +6,9 @@ var manager := OwnWar_BlockManager.new()
 
 
 func add_mesh(mesh: Mesh) -> void:
-	var node := MeshInstance.new()
-	node.mesh = mesh
-	node.layers = 0
+	var node = outline.add_outline_direct(mesh)
 	node.scale = Vector3(4, 4, 4)
-	add_child(node)
-	outline.add_outline(node)
+	node.translation = Vector3.ONE / 2.0
 	map[mesh] = node
 
 
@@ -22,18 +19,34 @@ func remove_mesh(mesh: Mesh) -> void:
 
 func add_node(position: Vector3, rotation: int, node: Spatial) -> void:
 	node = node.duplicate()
-	add_child(node)
 	var basis: Basis = manager.rotation_to_basis(rotation).scaled(Vector3(4, 4, 4))
-	node.transform = Transform(basis, position)
+	node.transform = Transform(basis, position + Vector3.ONE / 2.0)
+	var outline_nodes = []
 	for c in Util.get_children_recursive(node):
+		var n
 		if c is VisualInstance:
 			c.layers = 0
-		if c is MeshInstance:
-			outline.add_outline(c)
-	map[PoolIntArray([position.x, position.y, position.z])] = node
+			continue
+		elif c is MeshInstance:
+			n = outline.add_outline_direct(c)
+		# "Right operand of 'is' is not a class (type: 'NativeScript')"
+		# Thanks Godot
+		elif c.get_script() == BatchedMeshInstance:
+			n = outline.add_outline_direct(c)
+		else:
+			continue
+		var trf = c.transform
+		while c != node:
+			c = c.get_parent()
+			trf = c.transform * trf
+		n.transform = trf
+		outline_nodes.push_back(n)
+			
+	map[PoolIntArray([position.x, position.y, position.z])] = outline_nodes
 	
 
 func remove_node(position: Vector3) -> void:
 	var key := PoolIntArray([position.x, position.y, position.z])
-	map[key].queue_free()
+	for n in map[key]:
+		n.queue_free()
 	map.erase(key)
